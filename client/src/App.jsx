e2394c42f07e1c00b2c27b5225f3eb90e772f98b
rxjans <120579803+rxjans@users.noreply.github.com>
2024-06-03 13:47:23 +0530

    setup react-router-dom and routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,8 @@
 import { Button } from '@chakra-ui/react'
 import { useState } from 'react'
+import { Routes } from 'react-router-dom'
+import PostPage from './pages/PostPage'
+import UserPage from './pages/UserPage'
 
 
 function App() {
@@ -7,9 +10,14 @@ function App() {
 
   return (
     <>
-      <Button>
-        Hello
-      </Button>
+      <div className='flex justify-center'>
+        <div className='max-w-[620px] w-full'>
+          <Routes>
+            <Route path="/:username" element={<UserPagePage />} />
+            <Route path="/:username/post/:pID" element={<PostPage />} />
+          </Routes>
+        </div>
+      </div>
     </>
   )
 }
