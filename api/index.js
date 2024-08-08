15b5d73e700dfd29ae9460ab41f3e2a9db2a11e1
rxjans <rajan4.sharma2001@gmail.com>
2024-08-08 18:17:14 +0530

    create userController

--- api/index.js
+++ api/index.js
@@ -1,12 +1,21 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import connectDB from './database/connectDB.js';
+import cookieParser from 'cookie-parser';
+import userRoutes from '../api/routes/userRoutes.js';
 
 dotenv.config();
 connectDB();
 const app = express();
 
 const PORT = process.env.PORT || 4000;
+
+app.use(express.json());
+app.use(cookieParser());
+
+// Routes 
+app.use('/api/user', userRoutes);   
+
 app.listen(PORT, ()=>{
     console.log(`Server started at http://localhost:${PORT}`)
 });
