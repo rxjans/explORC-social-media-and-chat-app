cf696c584fe353e418f9fac70908e606cefbc617
rxjans <rajan4.sharma2001@gmail.com>
2024-11-12 17:24:24 +0530

    Create user signup

--- api/controllers/userController.js
+++ api/controllers/userController.js
@@ -1,5 +1,38 @@
-import User from '../models/userModel';
+import User from '../models/userModel.js';
+import { errorHandler } from '../util/errorHandler.js';
+import bcryptjs from 'bcryptjs';
 
 export const signupUser = async(req, res, next)=>{
+    try {
+        const {name, email, username, password } = req.body;
+        const user = await User.findOne({$or: [{email, username}]});
 
+        if(user){
+            return next(errorHandler(409, 'User with same username/email already exists'));
+        }
+        
+        const hashedPassword = await bcryptjs.hashSync(password, 10);
+
+        const newUser = new User({
+            name, 
+            email,
+            username,
+            password: hashedPassword
+        });
+
+        await newUser.save();
+        if(newUser){
+            res.status(200).json({
+                _id: newUser._id,
+                name: newUser.name,
+                email: newUser.email,
+                username: newUser.username,
+                message: "Signed up successfully"
+            });
+        }
+        else next(error);
+
+    } catch (error) {
+        next(error);
+    }
 }
